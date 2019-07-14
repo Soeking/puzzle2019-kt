15a7dd87f6f7001db82aac21ebd5bc2fc1bd03b4
Soeking <great.from.g@gmail.com>
2019-07-15 00:49:32 +0900

    rename

--- android/src/com/puzzle/dcs/AndroidLauncher.kt
+++ android/src/com/puzzle/dcs/AndroidLauncher.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import com.puzzle.dcs.game
 
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val config = AndroidApplicationConfiguration()
         config.useImmersiveMode = true
-        initialize(game(), config)
+        initialize(Game(), config)
     }
 }
--- core/src/com/puzzle/dcs/game.kt
+++ core/src/com/puzzle/dcs/game.kt
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class game : ApplicationAdapter() {
+class Game : ApplicationAdapter() {
     internal lateinit var batch: SpriteBatch
     internal lateinit var img: Texture
 
@@ -16,7 +16,7 @@ class game : ApplicationAdapter() {
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
         batch.draw(img, 0f, 0f)
