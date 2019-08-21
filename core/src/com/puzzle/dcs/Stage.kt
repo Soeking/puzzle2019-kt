5e371062d832b4b71006f51a4cf1eaba5da5790a
Soeking <great.from.g@gmail.com>
2019-08-21 15:45:08 +0900

    read json file

--- core/src/com/puzzle/dcs/Stage.kt
+++ core/src/com/puzzle/dcs/Stage.kt
@@ -1,8 +1,18 @@
 package com.puzzle.dcs
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.utils.Json
 
 class Stage(private val game: Core, private val fileName: String) : Screen {
+    private val file = Gdx.files.internal("stages/$fileName")
+    private val json = Json()
+    lateinit var stageData: StageData
+
+    init {
+        Gdx.app.log("Ori", "${file.readString()}")
+        stageData = json.fromJson(stageData::class.java, file.readString())
+    }
 
     override fun render(delta: Float) {
 
