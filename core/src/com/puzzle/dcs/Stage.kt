bdb9847c6c0b3052a649917becb77a0cfb9daab6
Soeking <great.from.g@gmail.com>
2019-08-23 18:08:55 +0900

    add input processor

--- core/src/com/puzzle/dcs/Stage.kt
+++ core/src/com/puzzle/dcs/Stage.kt
@@ -1,10 +1,11 @@
 package com.puzzle.dcs
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.utils.Json
 
-class Stage(private val game: Core, private val fileName: String) : Screen {
+class Stage(private val game: Core, private val fileName: String) : Screen, InputProcessor {
     private val file = Gdx.files.internal("stages/$fileName")
     private val json = Json()
     lateinit var stageData: StageData
@@ -16,6 +17,18 @@ class Stage(private val game: Core, private val fileName: String) : Screen {
 
     override fun render(delta: Float) {
 
+    }
+
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return true
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        return true
+    }
+
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        return true
     }
 
     override fun resize(width: Int, height: Int) {
@@ -40,5 +53,25 @@ class Stage(private val game: Core, private val fileName: String) : Screen {
 
     override fun dispose() {
 
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun keyTyped(character: Char): Boolean {
+        return false
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        return false
+    }
+
+    override fun mouseMoved(screenX: Int, screenY: Int): Boolean {
+        return false
+    }
+
+    override fun scrolled(amount: Int): Boolean {
+        return false
     }
 }
