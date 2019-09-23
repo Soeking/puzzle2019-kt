2c1c3f3fb7c85c4030ba907ad54f0013e1274348
Tim Olson <mr.kaito01@gmail.com>
2019-09-24 01:04:52 +0900

    draw stage

--- core/src/com/puzzle/dcs/PlayScreen.kt
+++ core/src/com/puzzle/dcs/PlayScreen.kt
@@ -5,30 +5,176 @@ import com.badlogic.gdx.InputProcessor
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.World
 import com.google.gson.Gson
 
 class PlayScreen(private val game: Core, private val fileName: String) : Screen, InputProcessor {
+    private val spriteBatch = SpriteBatch()
     private val file: FileHandle
     private val json = Gson()
     private lateinit var stageData: StageData
     private val gridSize = Gdx.graphics.width / 10f
+    private val world: World
+    private val wallSprite: Sprite
+    private val squareSprite: Sprite
+    private val triangleSprite: Sprite
+    private val ladderSprite: Sprite
+    private val playerSprite: Sprite
+    private val goalSprite: Sprite
+    private val dynamicDef = BodyDef()
+    private val staticDef = BodyDef()
+    private val kinematicDef = BodyDef()
+    private val wallBodies = mutableListOf<Body>()
+    private val squareBodies = mutableListOf<Body>()
+    private val triangleBodies = mutableListOf<Body>()
+    private val ladderBodies = mutableListOf<Body>()
+    private val playerBody: Body
+    private val goalBody: Body
 
     init {
+        world = World(Vector2(0f, -8f), true)
         file = Gdx.files.internal("stages/$fileName")
+        wallSprite = Sprite(Texture(Gdx.files.internal("images/wall.png")))
+        squareSprite = Sprite(Texture(Gdx.files.internal("images/square.png")))
+        triangleSprite = Sprite(Texture(Gdx.files.internal("images/triangle.png")))
+        ladderSprite = Sprite(Texture(Gdx.files.internal("images/ladder.png")))
+        playerSprite = Sprite(Texture(Gdx.files.internal("images/ball.png")))
+        goalSprite = Sprite(Texture(Gdx.files.internal("images/goal.png")))
+
+        wallSprite.setOrigin(0f, 0f)
+        wallSprite.setScale(gridSize / wallSprite.width)
+        squareSprite.setOrigin(0f, 0f)
+        squareSprite.setScale(gridSize / squareSprite.width)
+        triangleSprite.setOrigin(0f, 0f)
+        triangleSprite.setScale(gridSize / triangleSprite.width)
+        ladderSprite.setOrigin(0f, 0f)
+        ladderSprite.setScale(gridSize / ladderSprite.width)
+        playerSprite.setOrigin(0f, 0f)
+        playerSprite.setScale(gridSize / playerSprite.width / 1.5f)
+        goalSprite.setOrigin(0f, 0f)
+        goalSprite.setScale(gridSize / goalSprite.width)
+
+        dynamicDef.type = BodyDef.BodyType.DynamicBody
+        dynamicDef.position.set(0f, 0f)
+        staticDef.type = BodyDef.BodyType.StaticBody
+        staticDef.position.set(0f, 0f)
+        kinematicDef.type = BodyDef.BodyType.KinematicBody
+        kinematicDef.position.set(0f, 0f)
 
         if (file.exists()) {
             stageData = json.fromJson(file.readString(), StageData::class.java)
+        } else {
 
-            stageData.wall.forEach { }
-            stageData.square.forEach { }
-            stageData.triangle.forEach { }
-            stageData.ladder.forEach { }
+        }
+
+        stageData.wall.forEach {
+            it.x *= gridSize
+            it.y *= gridSize
+            val body = world.createBody(staticDef)
+            val sprite = wallSprite
+            //sprite.setPosition(it.x, it.y)
+            body.userData = sprite
+            body.setTransform(it.x, it.y, 0f)
+            wallBodies.add(body)
+        }
+        stageData.square.forEach {
+            it.x *= gridSize
+            it.y *= gridSize
+            val body = world.createBody(kinematicDef)
+            val sprite = squareSprite
+            //sprite.setPosition(it.x, it.y)
+            body.userData = sprite
+            body.setTransform(it.x, it.y, 0f)
+            squareBodies.add(body)
+        }
+        stageData.triangle.forEach {
+            it.x *= gridSize
+            it.y *= gridSize
+            val body = world.createBody(kinematicDef)
+            val sprite = triangleSprite
+            //sprite.setPosition(it.x, it.y)
+            body.userData = sprite
+            body.setTransform(it.x, it.y, 0f)
+            triangleBodies.add(body)
+        }
+        stageData.ladder.forEach {
+            it.x *= gridSize
+            it.y *= gridSize
+            val body = world.createBody(kinematicDef)
+            val sprite = ladderSprite
+            //sprite.setPosition(it.x, it.y)
+            body.userData = sprite
+            body.setTransform(it.x, it.y, 0f)
+            ladderBodies.add(body)
+        }
+        stageData.start.let {
+            it.x *= gridSize
+            it.y *= gridSize
+            val sprite = playerSprite
+            //sprite.setPosition(it.x, it.y)
+            playerBody = world.createBody(dynamicDef)
+            playerBody.userData = sprite
+            playerBody.setTransform(it.x, it.y, 0f)
+        }
+        stageData.goal.let {
+            it.x *= gridSize
+            it.y *= gridSize
+            val sprite = goalSprite
+            //sprite.setPosition(it.x, it.y)
+            goalBody = world.createBody(staticDef)
+            goalBody.userData = sprite
+            goalBody.setTransform(it.x, it.y, 0f)
         }
     }
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(0.1f, 0.4f, 0.8f, 0f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        spriteBatch.begin()
+        drawSprites()
+        spriteBatch.end()
+
+        world.step(Gdx.graphics.deltaTime, 0, 0)
+    }
+
+    fun drawSprites() {
+        wallBodies.forEach {
+            val sprite = wallSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
+        squareBodies.forEach {
+            val sprite = squareSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
+        triangleBodies.forEach {
+            val sprite = triangleSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
+        ladderBodies.forEach {
+            val sprite = ladderSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
+        playerBody.let {
+            val sprite = playerSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
+        goalBody.let {
+            val sprite = goalSprite
+            sprite.setPosition(it.position.x, it.position.y)
+            sprite.draw(spriteBatch)
+        }
     }
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
--- core/src/com/puzzle/dcs/StageData.kt
+++ core/src/com/puzzle/dcs/StageData.kt
@@ -1,49 +1,50 @@
 package com.puzzle.dcs
 
 data class StageData(
-        val wall: MutableList<Wall>,
-        val square: MutableList<Square>,
-        val triangle: MutableList<Triangle>,
-        val ladder: MutableList<Ladder>,
-        val start: Start,
-        val goal: Goal,
-        val gravityChange: List<Int>,
-        val switch: List<Int>
+    val wall: MutableList<Wall>,
+    val square: MutableList<Square>,
+    val triangle: MutableList<Triangle>,
+    val ladder: MutableList<Ladder>,
+    val start: Start,
+    val goal: Goal,
+    val gravityChange: List<Int>,
+    val switch: List<Int>
 )
 
 data class Wall(
-        val x: Float,
-        val y: Float
+    var x: Float,
+    var y: Float
 )
 
 data class Square(
-        var x: Float,
-        var y: Float,
-        val gravityID: Int,
-        var gravity: Int
+    var x: Float,
+    var y: Float,
+    val gravityID: Int,
+    var gravity: Int
 )
 
 data class Triangle(
-        var x: Float,
-        var y: Float,
-        val gravityID: Int,
-        var gravity: Int,
-        val rotate: Int
+    var x: Float,
+    var y: Float,
+    val gravityID: Int,
+    var gravity: Int,
+    val rotate: Int
 )
 
 data class Ladder(
-        var x: Float,
-        var y: Float,
-        val gravityID: Int,
-        var gravity: Int
+    var x: Float,
+    var y: Float,
+    val gravityID: Int,
+    var gravity: Int,
+    val rotate: Int
 )
 
 data class Start(
-        val x: Float,
-        val y: Float
+    var x: Float,
+    var y: Float
 )
 
 data class Goal(
-        val x: Float,
-        val y: Float
+    var x: Float,
+    var y: Float
 )
