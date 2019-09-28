79c8fcc793df89bc2e6f90dd1d016cd459225d95
Soeking <great.from.g@gmail.com>
2019-09-29 00:09:41 +0900

    add collision

--- core/src/com/puzzle/dcs/PlayScreen.kt
+++ core/src/com/puzzle/dcs/PlayScreen.kt
@@ -14,14 +14,15 @@ import com.badlogic.gdx.physics.box2d.*
 import com.google.gson.Gson
 
 class PlayScreen(private val game: Core, private val fileName: String) : Screen, InputProcessor {
-    private val camera:OrthographicCamera
+    private val camera: OrthographicCamera
     private val spriteBatch = SpriteBatch()
     private val file: FileHandle
     private val json = Gson()
     private lateinit var stageData: StageData
     private val gridSize = Gdx.graphics.width / 10f
+    private val halfGrid = gridSize / 2f
     private val world: World
-    private val renderer:Box2DDebugRenderer
+    private val renderer: Box2DDebugRenderer
     private val wallSprite: Sprite
     private val squareSprite: Sprite
     private val triangleSprite: Sprite
@@ -37,12 +38,19 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
     private val ladderBodies = mutableListOf<Body>()
     private val playerBody: Body
     private val goalBody: Body
+    private val circleShape: CircleShape
+    private val boxShape: PolygonShape
+    private val playerFixtureDef = FixtureDef()
+    private val wallFixtureDef = FixtureDef()
+    private val playerFixture: Fixture
 
     init {
         Box2D.init()
-        camera= OrthographicCamera()
+        camera = OrthographicCamera()
         world = World(Vector2(0f, -8f), true)
-        renderer= Box2DDebugRenderer()
+        renderer = Box2DDebugRenderer()
+        createCollision()
+
         file = Gdx.files.internal("stages/$fileName")
         wallSprite = Sprite(Texture(Gdx.files.internal("images/wall.png")))
         squareSprite = Sprite(Texture(Gdx.files.internal("images/square.png")))
@@ -71,6 +79,19 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
         kinematicDef.type = BodyDef.BodyType.KinematicBody
         kinematicDef.position.set(0f, 0f)
 
+        circleShape = CircleShape()
+        circleShape.radius = gridSize / 3f
+        boxShape = PolygonShape()
+        boxShape.setAsBox(gridSize / 2f, gridSize / 2f)
+        playerFixtureDef.shape = circleShape
+        playerFixtureDef.isSensor = true
+        playerFixtureDef.friction = 0.5f
+        playerFixtureDef.restitution = 0.1f
+        wallFixtureDef.shape = boxShape
+        wallFixtureDef.isSensor = true
+        wallFixtureDef.friction = 1f
+        wallFixtureDef.restitution = 0f
+
         if (file.exists()) {
             stageData = json.fromJson(file.readString(), StageData::class.java)
         } else {
@@ -85,6 +106,7 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
             //sprite.setPosition(it.x, it.y)
             body.userData = sprite
             body.setTransform(it.x, it.y, 0f)
+            body.createFixture(wallFixtureDef)
             wallBodies.add(body)
         }
         stageData.square.forEach {
@@ -124,7 +146,8 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
             //sprite.setPosition(it.x, it.y)
             playerBody = world.createBody(dynamicDef)
             playerBody.userData = sprite
-            playerBody.setTransform(it.x, it.y, 0f)
+            playerBody.setTransform(it.x, it.y + 10, 0f)
+            playerFixture = playerBody.createFixture(playerFixtureDef)
         }
         stageData.goal.let {
             it.x *= gridSize
@@ -137,9 +160,34 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
         }
     }
 
+    private fun createCollision() {
+        world.setContactListener(object : ContactListener {
+            override fun beginContact(contact: Contact?) {
+
+            }
+
+            override fun endContact(contact: Contact?) {
+
+            }
+
+            override fun preSolve(contact: Contact?, oldManifold: Manifold?) {
+
+            }
+
+            override fun postSolve(contact: Contact?, impulse: ContactImpulse?) {
+
+            }
+        })
+    }
+
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(0.1f, 0.4f, 0.8f, 0f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        if (world.contactCount > 0) {
+            world.contactList.forEach {
+                Gdx.app.log("contact", "${it.fixtureA.body.position},${it.fixtureB.body.position}")
+            }
+        }
 
         spriteBatch.begin()
         drawSprites()
@@ -147,38 +195,38 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen,
 
         camera.update()
         world.step(Gdx.graphics.deltaTime, 0, 0)
-        renderer.render(world,camera.combined)
+        renderer.render(world, camera.combined)
     }
 
-    fun drawSprites() {
+    private fun drawSprites() {
         wallBodies.forEach {
             val sprite = wallSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x - halfGrid, it.position.y - halfGrid)
             sprite.draw(spriteBatch)
         }
         squareBodies.forEach {
             val sprite = squareSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x-halfGrid, it.position.y-halfGrid)
             sprite.draw(spriteBatch)
         }
         triangleBodies.forEach {
             val sprite = triangleSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x-halfGrid, it.position.y-halfGrid)
             sprite.draw(spriteBatch)
         }
         ladderBodies.forEach {
             val sprite = ladderSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x-halfGrid, it.position.y-halfGrid)
             sprite.draw(spriteBatch)
         }
         playerBody.let {
             val sprite = playerSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x - gridSize / 3f, it.position.y - gridSize / 3f)
             sprite.draw(spriteBatch)
         }
         goalBody.let {
             val sprite = goalSprite
-            sprite.setPosition(it.position.x, it.position.y)
+            sprite.setPosition(it.position.x-halfGrid, it.position.y-halfGrid)
             sprite.draw(spriteBatch)
         }
     }
