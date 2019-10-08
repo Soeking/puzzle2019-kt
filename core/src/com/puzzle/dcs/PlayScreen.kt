1eb738d8e7182b3ae21a672fe9eebb2e7423098b
SUAN4423 <toyanonichan@yahoo.co.jp>
2019-10-08 17:30:00 +0900

    -5→-

--- core/src/com/puzzle/dcs/PlayScreen.kt
+++ core/src/com/puzzle/dcs/PlayScreen.kt
@@ -347,21 +347,22 @@ class PlayScreen(private val game: Core, private val fileName: String) : Screen 
             ochitattawa += (Gdx.graphics.deltaTime * 1000).toInt()
             bitmapFont2.draw(spriteBatch, "落っこちんな～？", Gdx.graphics.width / 3.0f, Gdx.graphics.height / 2.0f * 1.5f)
             if (ochita3) {
-                playerBody.setLinearVelocity((-5.0f - playerBody.position.x) * 5, (0.5f - playerBody.position.y) * 5)
-                if (playerBody.position.x <= -4.5f && playerBody.position.x >= -5.5f &&
+                playerBody.setLinearVelocity((-1.0f - playerBody.position.x) * 5, (0.5f - playerBody.position.y) * 5)
+                if (playerBody.position.x <= -0.5f && playerBody.position.x >= -1.5f &&
                         playerBody.position.y <= 1.0f && playerBody.position.y >= 0.0f) {
                     ochita3 = false
                 }
             }
             else if (ochita2) {
-                playerBody.setLinearVelocity((-5.0f - playerBody.position.x) * 5, (10.0f - playerBody.position.y) * 5)
-                if (playerBody.position.x <= -4.5f && playerBody.position.x >= -5.5f &&
+                playerBody.setLinearVelocity((-1.0f - playerBody.position.x) * 5, (10.0f - playerBody.position.y) * 5)
+                if (playerBody.position.x <= -0.5f && playerBody.position.x >= -1.5f &&
                         playerBody.position.y <= 10.5f && playerBody.position.y >= 9.5f) {
                     ochita2 = false
                 }
             }
             else if (ochita) {
                 playerBody.setLinearVelocity((10.0f - playerBody.position.x) * 5, (10.0f - playerBody.position.y) * 5)
+                playerBody.angularVelocity = -45.0f
                 if (playerBody.position.x <= 10.5f && playerBody.position.x >= 9.5f &&
                         playerBody.position.y <= 10.5f && playerBody.position.y >= 9.5f) {
                     ochita = false
